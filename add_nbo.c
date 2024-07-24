a2362f0c30b38fe1aa66b9ac4ba037ad109300cd
shlee__ <102645357+oIfloraIo@users.noreply.github.com>
2024-07-24 23:02:32 +0900

    Update add_nbo.c

--- add_nbo.c
+++ add_nbo.c
@@ -1,20 +1,7 @@
-#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
-
-void dump(void* p, size_t n) {
-    uint8_t* u8 = static_cast<uint8_t*>(p);
-    size_t i = 0;
-    while (true) {
-        printf("%02X ", *u8++);
-        if (++i >= n) break;
-        if (i % 8 == 0)
-            printf("\n");
-    }
-    printf("\n");
-}
 
 uint32_t read_uint32_from_file(const char* filename) {
     FILE* file = fopen(filename, "rb");
